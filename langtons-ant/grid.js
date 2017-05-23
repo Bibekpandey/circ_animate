4feee99b1e63548d024a46134792bf44f24a052f
Bibek Pandey <bibek@janakitech.com>
2017-05-23 18:28:31 +0545

    working on doubling size

--- langtons-ant/grid.js
+++ langtons-ant/grid.js
@@ -1,7 +1,7 @@
 function Grid(canvasid, rule) {
     var UP=0,LEFT=1,DOWN=2,RIGHT=3;
-    var cols = 20; // TODO: from parameters
-    var rows = 20; // TODO: from parameters
+    var cols = 2; // TODO: from parameters
+    var rows = 2; // TODO: from parameters
     var grid_size = 32; // 32 px grid size
     this.fps = 60;
     var pos = {x:0,y:0};
@@ -11,6 +11,9 @@ function Grid(canvasid, rule) {
     var canvas, context, elements;
 
     var color = ['white', 'red'];
+    this.printelements = function() {
+        console.log(elements);
+    }
 
     this.initialize = function() {
         'use strict';
@@ -22,36 +25,61 @@ function Grid(canvasid, rule) {
         for(var x=0;x<elements.length;x++)elements[x]=0;
         context.fillStyle="#aaaaaa"; // light grey
         context.fillRect(0,0,canvas.width, canvas.height);
-        setInterval(this.update, 20);
+        elements[1] = 1;
+        //setInterval(this.update, 2000);
+    }
+    this.renderElements = function() {
+        for(var x=0;x<elements.length;x++) {
+            renderNth(x);
+        }
     }
 
-    function doublesize() {
+    this.doubleSize = function() {
         grid_size/=2;
+        var oldcols = cols;
+        var oldrows = rows;
         cols*=2;
         rows*=2;
         var new_elements = new Array(cols*rows);
-        for(var x=0;x<new_elements.length;x++) {
-            if(x<elements.length) {
-                new_elements[x]=elements[x];
+
+        // first set all to zero
+        for(var i=0;i<rows*cols;i++) new_elements[i] = 0;
+
+        // copy old elements
+        for(var y=0;y<oldrows;y++) {
+            for(var x=0;x<oldcols;x++) {
+                var oldindex = y*oldcols+x;
+                var newindex = (oldcols/2+y)*cols + oldrows/2+x;
+                new_elements[newindex] = elements[oldindex];
             }
-            else new_elements[x] = 0;
         }
+        elements = new_elements;
+
         var oldpos = pos;
+        pos = {x:oldcols/2+oldpos.x,y:oldrows/2+y};
+
         var olddir = dir;
         // render old elements
         // clear first
         context.fillStyle="#aaaaaa"; // light grey
         context.fillRect(0,0,canvas.width, canvas.height);
         // now fill elements
-        for(var x=0;x<elements.length;x++) {
-            renderNth(x);
+        for(var y=0;y<oldrows;y++) {
+            for(var x=0;x<oldcols;x++) {
+                var newindex = (oldcols/2+y)*cols + oldrows/2+x;
+                var state = elements[newindex];
+                var p = {x:oldcols/2+x, y:oldrows/2+y};
+                renderCell(p, state);
+            }
         }
-
-        elements = new_elements;
     }
 
-    function renderNth(index) {
+    this.renderNth = function (index) {
         // first get position of the indexed elem
+        var x = index%cols;
+        var y = parseInt(index/cols);
+        console.log('render', x,y, elements[index]);
+        renderCell({x:x,y:y}, elements[index]);
     }
 
     var position = function(pos) { // translate origin to center
@@ -62,6 +90,7 @@ function Grid(canvasid, rule) {
         context.fillStyle = color[state];
         var x1 = pos.x*grid_size;
         var y1 = pos.y*grid_size;
+        console.log('cell', x1, y1, grid_size);
         var newpos = position({x:x1, y:y1});
         context.fillRect(newpos.x,newpos.y, grid_size, grid_size);
     }
@@ -116,22 +145,24 @@ function Grid(canvasid, rule) {
         elements[(rows/2-pos.y)*cols+cols/2+pos.x] = state;
     }
 
+    function checkBoundary() {
+        // TODO: check if ant hits boundary
+        return false;
+    }
+
     this.update = function() {
         var state = getState(pos);
-        console.log('oldstate', state);
-        console.log('oldpos', pos);
-        console.log('olddir', dir);
         var newstate = rule[state].state;
         var dirn;
         renderCell(pos, newstate);
         setState(pos, newstate);
-        console.log('newstate', getState(pos));
         var move = rule[state].move;
-        console.log('move', move);
         direction[move]();
-        console.log('newpos', pos);
-        console.log('newdir', dir);
-        console.log();
+
+        // check if boundary
+        if(checkBoundary()) {
+            doubleSize();
+        }
     }
 
     var renderGrid = function() {
