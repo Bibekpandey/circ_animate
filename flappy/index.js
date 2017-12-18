ca8fe6393435ff282cb27a51330bb73630738637
Bibek Pandey <bibek@janakitech.com>
2017-12-18 14:59:16 +0545

    major refactor

--- flappy/index.js
+++ flappy/index.js
@@ -2,165 +2,181 @@ const canvas = document.getElementById('flappy');
 const context = canvas.getContext('2d');
 const scoreElem = document.getElementById('score');
 
-const globals = {
-    game_running: false,
+const globalConfig = {
     vertical_spacing: 175,
-    gravity: 10,
     horizontal_spacing: 375,
+    fps : 60,
+    pipes_stored: 5,
     background: 'black',
-    total_pipes: 5,
-    counter: 0,
-    width: canvas.width,
-    height: canvas.height,
-    pipe_width: 75,
-    interval_id: 0,
-    bird_size: 20,
-    bird_color: 'green',
-    pipe_color: 'red',
-    speed: 20,
+    ay: 0.17,
     vx: 10,
-    vy: 10,
-    ay: 1.7,
-    score: 0,
-    scoreUpdate: true
+    pipe_width: 75,
+}
+
+class PipePair {
+    constructor(x) {
+        this.width = 75;
+        this.color = 'red';
+
+        const top_height = parseInt(Math.random()*canvas.height/2) + 50;
+
+        this.top_height = top_height;
+        this.bottom_height = this.top_height + globalConfig.vertical_spacing;
+        this.x = x;
+    }
+
+    render() {
+        context.fillStyle = this.color;
+        context.fillRect(
+            this.x,
+            0,
+            globalConfig.pipe_width,
+            this.top_height
+        );
+        context.fillRect(
+            this.x,
+            this.bottom_height,
+            globalConfig.pipe_width,
+            canvas.height - this.bottom_height,
+        );
+    }
+    updatePosition ()  {
+        this.x -= globalConfig.vx;
+        return this;
+    }
 };
 
-const elements = {
-    pipes: [],
-};
-
-
-function start () {
-    globals.game_running=true;
-    globals.score = 0;
-    scoreElem.innerHTML = 0;
-    initialize();
-    globals.interval_id = setInterval(update, 1000/globals.speed);
-}
-
-function initialize() {
-    elements.pipes = [];
-    let img = new Image();
-    img.src='sprites.png';
-    elements.bird= {
-        x: 100,
-        y: globals.height/2,
-        vy: 0,
-        img: img
+class Bird {
+    constructor(x=200, y=canvas.height/2, size=20) {
+        this.alive  = true;
+        this.size = size;
+        this.color = 'green';
+        this.vx = 10;
+        this.vy = 10;
+        this.score = 0;
+        this.scoreUpdate = true;
+        this.x = x;
+        this.y = y;
     }
-    // first initialize with 5 pipe pairs
-    elements.pipes = Array.from(Array(globals.total_pipes).keys()).map(
-        (x, i) => get_pipes_pair(i)
-    );
-}
-
-function get_pipes_pair(x) {
-    const top_height = parseInt(Math.random()*globals.height/2) + 50;
-    return {
-        top_height: top_height,
-        bottom_height: top_height + globals.vertical_spacing,
-        x_pos: globals.width - globals.pipe_width + x*globals.horizontal_spacing,
+    render()  {
+        context.fillStyle = this.color;
+        /*context.drawImage(*/
+            //elements.bird.img,
+            //elements.bird.x,
+            //elements.bird.y,
+            //globals.bird_size*1.33,
+            //globals.bird_size
+        /*)*/
+        context.fillRect(this.x,this.y, this.size, this.size);
+    }
+    updatePosition ()  {
+        this.vy += globalConfig.ay;
+        this.y += this.vy;
+        return this;
     }
 }
 
-function update() {
-    updateBirdPosition();
-    updatePipesPosition();
-    spawnPipe();
-    renderElements();
-    if (checkCollision()) {
-        clearInterval(globals.interval_id);
-        globals.game_running = false;
-        return;
+class Game {
+    constructor() {
+        this.interval_id = 0;
+        this.elements = {};
+
+        // bind all methods
+        this.start = this.start.bind(this);
+        this.initialize = this.initialize.bind(this);
+        this.update = this.update.bind(this);
+        this.spawnPipe = this.spawnPipe.bind(this);
+        this.renderElements = this.renderElements.bind(this);
+        this.renderBackground = this.renderBackground.bind(this);
+        this.checkCollision = this.checkCollision.bind(this);
+        this.updateScore = this.updateScore.bind(this);
     }
-    updateScore();
-    //requestAnimationFrame(update);
-}
 
-function spawnPipe() {
-    if(elements.pipes[0].x_pos < -globals.pipe_width) {
-        // remove it from the list
-        elements.pipes.splice(0, 1);
-        // add new to last position
-        elements.pipes.push(get_pipes_pair(globals.total_pipes-2));
-        globals.scoreUpdate = true;
+    start () {
+        this.initialize();
+        this.interval_id = setInterval(this.update, globalConfig.fps);
     }
-}
 
-function renderElements() {
-    context.clearRect(0,0, globals.width, globals.height);
-    renderBackground();
-
-    renderBird();
-    renderPipes();
-}
-
-function renderBackground() {
-    context.fillStyle = globals.background;
-    context.fillRect(0,0, canvas.width, canvas.height);
-}
-
-function renderBird() {
-    context.fillStyle = globals.bird_color;
-    /*context.drawImage(*/
-        //elements.bird.img,
-        //elements.bird.x,
-        //elements.bird.y,
-        //globals.bird_size*1.33,
-        //globals.bird_size
-    /*)*/
-    context.fillRect(elements.bird.x,elements.bird.y, globals.bird_size, globals.bird_size);
-}
-
-function renderPipes() {
-    for (let x=0;x<elements.pipes.length;x++) {
-        renderPipe(elements.pipes[x]);
+    initialize ()  {
+        this.elements.bird =  new Bird();
+        // first initialize with 5 pipe pairs
+        const pipestart = 2*canvas.width/3;
+        this.elements.pipes = Array.from(Array(globalConfig.pipes_stored).keys()).map(
+            (x, i) => {
+                return new PipePair(pipestart + i*globalConfig.horizontal_spacing)
+            }
+        );
     }
-}
 
-function renderPipe(pipe) {
-    context.fillStyle = globals.pipe_color;
-    context.fillRect(pipe.x_pos,0, globals.pipe_width, pipe.top_height);
-    context.fillRect(pipe.x_pos, pipe.bottom_height, globals.pipe_width, globals.height, pipe.bottom_height);
-}
+    update ()  {
+        this.renderElements();
+        this.elements.bird = this.elements.bird.updatePosition(); // get new bird with new position
+        this.elements.pipes = this.elements.pipes.map((x, i) => x.updatePosition());
+        this.spawnPipe();
+        this.renderElements();
+        if (this.checkCollision()) {
+            clearInterval(this.interval_id);
+            globalConfig.game_running = false;
+            return;
+        }
+        this.updateScore();
+    }
 
-function checkCollision() {
-    if(elements.bird.y <=0 || elements.bird.y >= globals.height) return true;
-    // check if any of the corner of the bird(rectangle) falls within pipes(check first pipe only)
-    const size = globals.bird_size;
-    let corners = [
-        {x: elements.bird.x, y:elements.bird.y},
-        {x: elements.bird.x+size, y:elements.bird.y+size},
-        {x: elements.bird.x, y:elements.bird.y+size},
-        {x: elements.bird.x+size, y:elements.bird.y},
-    ]
-    // get first pipe
-    const pipe = elements.pipes[0];
-    for(let x=0;x<corners.length;x++) {
-        if(corners[x].x >= pipe.x_pos && corners[x].x <= pipe.x_pos + globals.pipe_width) {
-            if (corners[x].y >= 0 && corners[x].y <= pipe.top_height) return true;
-            if (corners[x].y >= pipe.bottom_height && corners[x].y <= globals.height) return true;
+    spawnPipe ()  {
+        if(this.elements.pipes[0].x < -globalConfig.pipe_width) {
+            // remove it from the list
+            this.elements.pipes.splice(0, 1);
+            // add new to last position
+            const pipe = new PipePair(
+                this.elements.pipes[globalConfig.pipes_stored-2].x + globalConfig.pipe_width + globalConfig.horizontal_spacing
+            )
+            this.elements.pipes.push(pipe)
+            // TODO
+            globalConfig.scoreUpdate = true;
         }
     }
-    return false;
-}
-function updateScore() {
-    if(elements.bird.x > (elements.pipes[0].x_pos + globals.pipe_width)) {
-        if(globals.scoreUpdate) {
-            globals.score += 1;
-            scoreElem.innerHTML = globals.score;
-            globals.scoreUpdate = false;
-        }
+
+    renderElements ()  {
+        context.clearRect(0,0, globalConfig.width, globalConfig.height);
+        this.renderBackground();
+
+        this.elements.bird.render();
+        this.elements.pipes.map(x => x.render());
     }
-}
 
-function updateBirdPosition() {
-    elements.bird.vy += globals.ay;
-    elements.bird.y += elements.bird.vy;
-}
+    renderBackground ()  {
+        context.fillStyle = globalConfig.background;
+        context.fillRect(0,0, canvas.width, canvas.height);
+    }
 
-function updatePipesPosition() {
-    for (let x=0;x<elements.pipes.length;x++) {
-        elements.pipes[x].x_pos -= globals.vx;
+    checkCollision ()  {
+        if(this.elements.bird.y <=0 || this.elements.bird.y >= globalConfig.height) return true;
+        // check if any of the corner of the bird(rectangle) falls within pipes(check first pipe only)
+        const size = this.elements.bird.size;
+        let corners = [
+            {x: this.elements.bird.x, y:this.elements.bird.y},
+            {x: this.elements.bird.x+size, y:this.elements.bird.y+size},
+            {x: this.elements.bird.x, y:this.elements.bird.y+size},
+            {x: this.elements.bird.x+size, y:this.elements.bird.y},
+        ]
+        // get first pipe
+        const pipe = this.elements.pipes[0];
+        for(let x=0;x<corners.length;x++) {
+            if(corners[x].x >= pipe.x && corners[x].x <= pipe.x + globalConfig.pipe_width) {
+                if (corners[x].y >= 0 && corners[x].y <= pipe.top_height) return true;
+                if (corners[x].y >= pipe.bottom_height && corners[x].y <= globalConfig.height) return true;
+            }
+        }
+        return false;
+    }
+
+    updateScore ()  {
+        if(this.elements.bird.x > (this.elements.pipes[0].x_pos + globalConfig.pipe_width)) {
+            if(globalConfig.scoreUpdate) {
+                globalConfig.score += 1;
+                scoreElem.innerHTML = globalConfig.score;
+                globalConfig.scoreUpdate = false;
+            }
+        }
     }
 }
