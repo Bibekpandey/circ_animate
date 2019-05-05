84e9dd98c1d8f6812a5d846294ede06625b661c4
bewakes <spirit_bibek@yahoo.com>
2019-05-05 12:20:08 +0545

    Basic cells movement

--- thanos-effect/index.js
+++ thanos-effect/index.js
@@ -1,72 +1,110 @@
-export function getSplittedImageData(canvas, cellWidth=5, cellHeight=5) {
+export class Cell {
+    constructor(state) {
+        this.state = {
+            vx: Math.random() > 0.3 ? 4 : 1, // default vx
+            vy: Math.random() > 0.5 ? 1 : 0, // default vy
+            ...state,
+        };
+        this.write = this.write.bind(this);
+        this.update = this.update.bind(this);
+    }
+
+    write(ctx, newImageData, colorArr=[255,255,255,255]) {
+        const { width, height } = ctx.canvas;
+        const { height: cellHeight, width: cellWidth } = this.state;
+        const { x, y, pixelsArray } = this.state;
+        pixelsArray.forEach((pixelRow, rowIndex) => {
+            pixelRow.forEach((pixel, colIndex) => {
+                if(x * cellWidth > width || y * cellHeight > height) return;
+                const ind = (x*cellWidth + (y*cellHeight+rowIndex)*width) * 4 + colIndex;
+                newImageData.data[ind] = colorArr[0];
+                newImageData.data[ind+1] = colorArr[1];
+                newImageData.data[ind+2] = colorArr[2];
+                newImageData.data[ind+3] = 255;
+            });
+        });
+        return newImageData;
+    }
+
+    update() {
+        const newState = {
+            ...this.state,
+            x: this.state.x + this.state.vx,
+            y: this.state.y + this.state.vy,
+        };
+        return new Cell(newState);
+    }
+}
+
+export function getCellsFromCanvas(canvas, cellWidth=5, cellHeight=5) {
     const { width, height } = canvas;
 
     if(width % cellWidth !== 0 || height % cellHeight !== 0) {
         throw "cell width/height does not divide canvas width/height";
     }
-    const X = width / cellWidth;
-    const Y = height / cellHeight;
-
     const ctx = canvas.getContext('2d');
     const imageData = ctx.getImageData(0, 0, width, height).data;
+
     const cells = [];
-    for (let y = 0; y < Y; y++) {
-        for(let x = 0;x < X; x++) {
-            const cellData = extractCellFromImageData(imageData, x, y, cellWidth, cellHeight, width, height);
-            cells.push(cellData);
+
+    for(let y=0;y<height;y+=cellHeight) {
+        for (let x=0;x<width;x+=cellWidth) {
+            const cellX = x/cellWidth, cellY = y/cellHeight;
+            const pixelsArray = [];
+
+            for(let cy=0;cy<cellHeight;cy++) {
+                const pixelRow = [];
+                for(let cx=0;cx<cellWidth;cx++) {
+                    pixelRow.push(
+                        imageData[(y+cy)*width + (x+cx)],
+                        imageData[(y+cy)*width + (x+cx)+1],
+                        imageData[(y+cy)*width + (x+cx)+2],
+                        imageData[(y+cy)*width + (x+cx)+3],
+                    );
+                }
+                pixelsArray.push(pixelRow);
+            }
+            cells.push(
+                new Cell({
+                    x: cellX,
+                    y: cellY,
+                    height: cellHeight,
+                    width: cellWidth,
+                    pixelsArray,
+                })
+            );
         }
     }
     return cells;
 }
 
-function extractCellFromImageData(imageData, xpos, ypos, cellWidth, cellHeight, width, height) {
-    const cellPixels = [];
-    for (let y=0;y<cellHeight;y++) {
-        for (let x=0;x<cellWidth;x++) {
-            const imageX = xpos*cellWidth + x;
-            const imageY = ypos*cellHeight + y;
-            const imageDataIndex = imageY * width + imageX;
-            cellPixels.push({
-                x: imageX,
-                y: imageY,
-                data: [ // R, G, B, A
-                    imageData[imageDataIndex], imageData[imageDataIndex+1],
-                    imageData[imageDataIndex+2], imageData[imageDataIndex+3],
-                ],
-            });
-        }
-    }
-    return cellPixels;
-}
-
-export function renderCell(ctx, cell, colorArr, newImageData) {
+export function writeCell(ctx, cell, colorArr, newImageData, cellWidth, cellHeight) {
     const { width, height } = ctx.canvas;
-    cell.forEach(pixel => {
-        const ind = (pixel.x + pixel.y*width) * 4;
-        newImageData.data[ind] = colorArr[0];
-        newImageData.data[ind+1] = colorArr[1];
-        newImageData.data[ind+2] = colorArr[2];
-        newImageData.data[ind+3] = colorArr[3] || 255;
+    const { x, y, pixelsArray } = cell.state;
+    pixelsArray.forEach((pixelRow, rowIndex) => {
+        pixelRow.forEach((pixel, colIndex) => {
+            const ind = (x*cellWidth + (y*cellHeight+rowIndex)*width) * 4 + colIndex;
+            newImageData.data[ind] = colorArr[0];
+            newImageData.data[ind+1] = colorArr[1];
+            newImageData.data[ind+2] = colorArr[2];
+            newImageData.data[ind+3] = 255;
+        });
+        
     });
     return newImageData;
 }
 
-export function renderCells(ctx, cells, color='white') {
-    const { width, height } = ctx.canvas;
-    const imageData = ctx.getImageData(0, 0, width, height);
+export function run(ctx, cells, fps=60, t=1) {
+    if(t > 200) return;
 
-    const newImageData = cells.reduce((imageData, cell) => {
-        let color = 255;
-        if (Math.random() > 0.5) {
-            color = 122;
-        }
-        const colorArr = [color, color, color, 255];
-        return renderCell(ctx, cell, colorArr, imageData);
-    }, imageData);
-        
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    const newImageData = cells.reduce(
+        (imgData, cell) => cell.write(ctx, imgData),
+        ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height),
+    );
+
     ctx.putImageData(newImageData, 0, 0);
-}
+    const newCells = cells.map(cell =>  cell.update());
 
-
-export function doThanos(ctx, cells) {
+    setTimeout(run, 1000/fps, ctx, newCells, fps, t+1);
 }
